032400768abac5ae9037bd6cb4ef3af0ca493152
Mai Huy Hoang <maihuyhoang97@gmail.com>
2021-04-19 22:53:46 +0700

    Fix scripts

--- scripts/run_api_docker.sh
+++ scripts/run_api_docker.sh
@@ -3,4 +3,4 @@ docker container stop tuike_api
 echo "Remove tuike_api"
 docker container rm tuike_api
 echo "Run tuike_api"
-docker run -p 8000:8000 --env "PORT=8000" --env-file env.list --name tuike_api -d tuike_api
+docker run -p 8000:8000 --env "PORT=8000" --env-file env.list --name tuike_api -d hoangmaihuy/tuike_api
--- scripts/run_consumer_docker.sh
+++ scripts/run_consumer_docker.sh
@@ -3,4 +3,4 @@ docker container stop tuike_consumer
 echo "Remove tuike_consumer"
 docker container rm tuike_consumer
 echo "Run tuike_consumer"
-docker run --env-file env.list --name tuike_consumer -d tuike_consumer
+docker run --env-file env.list --name tuike_consumer -d hoangmaihuy/tuike_consumer
