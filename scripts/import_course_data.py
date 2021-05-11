83191de7ab7a6ed5fb710ba0265dd3515f8ca9d7
Mai Huy Hoang <maihuyhoang97@gmail.com>
2021-05-11 15:38:41 +0700

    Import teacher and class data

--- scripts/import_course_data.py
+++ scripts/import_course_data.py
@@ -6,7 +6,8 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tuike_api.settings")
 django.setup()
 
 from course_service.consts import CourseTypeEnum, SchoolEnum
-from course_service.models import Course, Class, Teacher
+from course_service.models import Course, Class
+from teacher_service.models import Teacher
 from common.utils import TimeUtils
 import simplejson as json
 
@@ -22,11 +23,14 @@ COURSE_TYPE_MAP = {
 	"trans_choice": "通选课",
 }
 
+
+
 def import_courses(course_type):
 	filename = os.path.join(os.getcwd(), COURSE_DATA_DIR, course_type + '.json')
 	with open(filename, "r", encoding='utf8') as f:
 		courses = json.load(f)
 	course_objs = []
+	teacher_names = []
 	for course in courses:
 		school_id = SchoolEnum.get(course['school'])
 		if school_id is None:
@@ -42,7 +46,8 @@ def import_courses(course_type):
 			print("No type_id for {}".format(type_name))
 			continue
 		credit = course['credits'] if course_type in ['english', 'trans_choice'] else course['credit']
-		c = Course(
+		teacher_names.extend(course['teachers'])
+		course_obj = Course(
 			name=course['name'],
 			course_no=course['id'],
 			credit=credit,
@@ -52,16 +57,48 @@ def import_courses(course_type):
 			last_review=0,
 			create_time=TimeUtils.now_ts(),
 		)
-		course_objs.append(c)
-	print("bulk_create {}".format(course_type))
+		course_objs.append(course_obj)
+	print("bulk_create courses {}".format(course_type))
 	Course.objects.bulk_create(course_objs)
-	print("bulk_create finished")
+	print("bulk_create courses finished")
+
+	teacher_names = list(set(teacher_names))
+	created_names = set(Teacher.objects.filter(name__in=teacher_names).values_list("name", flat=True))
+	teacher_objs = [
+		Teacher(
+			name=name,
+			review_count=0,
+			create_time=TimeUtils.now_ts()
+		)
+		for name in teacher_names if name not in created_names
+	]
+	print("bulk_create teachers")
+	Teacher.objects.bulk_create(teacher_objs)
+	print("bulk_create teachers finished")
+
+	class_objs = []
+
+	for course in courses:
+		school_id = SchoolEnum.get(course['school'])
+		course_id = Course.objects.filter(name=course['name'], school_id=school_id).first().id
+		teachers = list(Teacher.objects.filter(name__in=course['teachers']))
+		for teacher in teachers:
+			class_objs.append(Class(
+				course_id=course_id,
+				teacher_id=teacher.id,
+				semester="20-21-2",
+				review_count=0,
+				create_time=TimeUtils.now_ts()
+			))
+	print("bulk_create classes")
+	Class.objects.bulk_create(class_objs)
+	print("bulk_create classes finished")
 
 if __name__ == '__main__':
-	# import_courses('speciality')
-	# import_courses('gym')
-	# import_courses('english')
+	import_courses('speciality')
+	import_courses('gym')
+	import_courses('english')
 	import_courses('liberal_computer')
-	# import_courses('politics')
-	# import_courses('pub_choice')
-	# import_courses('trans_choice')
+	import_courses('politics')
+	import_courses('pub_choice')
+	import_courses('trans_choice')
--- teacher_service/models.py
+++ teacher_service/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class Teacher(models.Model):
 	id = models.AutoField(primary_key=True)
-	name = models.CharField(max_length=20)
-	review_count = models.PositiveIntegerField()
+	name = models.CharField(max_length=50)
+	review_count = models.PositiveIntegerField(default=0)
 	recommend_score = models.FloatField(default=0)
 	exam_score = models.FloatField(default=0)
 	work_score = models.FloatField(default=0)
