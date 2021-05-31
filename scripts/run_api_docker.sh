986b3f50a4ebda2e3e423f1ead2842fe01890bf4
Mai Huy Hoang <maihuyhoang97@gmail.com>
2021-05-31 21:34:17 +0700

    Add pull tuike_api in script

--- scripts/run_api_docker.sh
+++ scripts/run_api_docker.sh
@@ -2,5 +2,7 @@ echo "Stop tuike_api"
 docker container stop tuike_api
 echo "Remove tuike_api"
 docker container rm tuike_api
+echo "Pull tuike_api"
+docker pull hoangmaihuy/tuike_api
 echo "Run tuike_api on port 3002"
 docker run -p 3002:8000 --env "PORT=8000" --env-file env.list --name tuike_api -d hoangmaihuy/tuike_api
